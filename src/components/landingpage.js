508112a8828b503cadb20273bc4e6b2af70b38eb
Yichen Ma <yichenm2@uci.edu>
2019-03-27 16:18:31 -0700

    Landing Page

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,9 +1,41 @@
 import React, {Component} from 'react';
+import {Grid,Cell} from "react-mdl"
 
 class Landing extends Component {
         render() {
                 return (
-                        <div><h1>Landing Page</h1></div>
+                        <div style={{width:'100%',margin:'auto'}}>
+                                <Grid className='landing-grid'>
+                                        <Cell col={12}>
+                                                <img
+                                                        src = "https://cdn1.iconfinder.com/data/icons/user-pictures/101/malecostume-512.png"
+                                                        alt = "avatar"
+                                                        className = "avatar-img"
+
+                                                />
+                                                <div className="banner-text">
+                                                        <h1>Full Stack web developer</h1>
+                                                <hr/>
+                                                <p>PYTHON | JAVA | REACT</p>
+                                                <div className="social-links">
+                                                        {/* LinkedIn */}
+                                                        <a href="http://google.com" rel="noopener noreferrer" target="_blank"></a>
+                                                        <i className="fa fa-linkedin-square"  aria-hidden="true"/>
+                                                        {/* GitHub */}
+                                                        <a href="http://google.com" rel="noopener noreferrer" target="_blank"></a>
+                                                        <i className="fa fa-github-square"  aria-hidden="true"/>
+                                                        {/* Freecodecamp */}
+                                                        <a href="http://google.com" rel="noopener noreferrer" target="_blank"></a>
+                                                        <i className="fa fa-free-code-camp"  aria-hidden="true"/>
+                                                        {/* Youtube */}
+                                                        <a href="http://google.com" rel="noopener noreferrer" target="_blank"></a>
+                                                        <i className="fa fa-youtube-square"  aria-hidden="true"/>
+                                                </div>
+                                                
+                                                </div>
+                                        </Cell>
+                                </Grid>
+                        </div>
                 )
         }
 }
